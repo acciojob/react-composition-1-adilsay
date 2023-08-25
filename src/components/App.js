84932cf6530e0e9440efc6fe2ceacb63ddb306f8
adilsay <77444860+adilsay@users.noreply.github.com>
2023-08-25 10:20:27 +0000

    code additional

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from "./Tabs"
 
 const App = () => {
+   let tabs = [{"title" : "Tab 1", "content" : "This is a content of Tab 1" },
+   {"title" : "Tab 2", "content" : "This is a content of Tab 2" },
+   {"title" : "Tab 3", "content" : "This is a content of Tab 3" }
+  
+          ]
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Tabs tabs={tabs}/>
+              {/* Do not remove the main div */}
     </div>
   )
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,22 @@
+import React,{useState} from "react";
+
+
+const Tabs = (props)=>{
+        console.log(typeof(props));
+        const [activeTab, setActiveTab] = useState(props.tabs[0].title);
+        function displayContent(title){
+            setActiveTab(title);
+        }
+
+    return (
+        <div>
+            {props.tabs.map(tab=>(<li onClick={()=> displayContent(tab.title)}>{tab.title}</li>))}
+            <div>
+                {props.tabs.filter(tab=>tab.title===activeTab)[0].content}
+            </div>
+
+        </div>
+    )
+}
+
+export default Tabs;
